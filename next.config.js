c201287dc2f4f515e9fd926def9cd513fa25ee03
Yaşar İçli <yasaricli@gmail.com>
2023-06-06 01:30:47 +0300

    google-analytics added and progressbar

--- next.config.js
+++ next.config.js
@@ -1,13 +1,17 @@
 const path = require("path");
 
-const { API_URL } = process.env;
+const { API_URL, PROGRESSBAR_COLOR, GOOGLE_ANALYTICS_CODE } = process.env;
 
 const serverRuntimeConfig = {
   API_URL,
+  PROGRESSBAR_COLOR,
+  GOOGLE_ANALYTICS_CODE,
 };
 
 const publicRuntimeConfig = {
   API_URL,
+  PROGRESSBAR_COLOR,
+  GOOGLE_ANALYTICS_CODE,
 };
 
 module.exports = {
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import { useEffect } from "react";
 import Head from "next/head";
+import Script from "next/script";
+import getConfig from "next/config";
+
+import NextNProgress from "nextjs-progressbar";
 import { config } from "@fortawesome/fontawesome-svg-core";
 
 // Styles
@@ -7,6 +11,9 @@ import "../styles/main.scss";
 
 // Configs
 config.autoAddCss = false;
+
+// Public Runtime Config See: next.config.js and .env, .env.production files.
+const { publicRuntimeConfig } = getConfig();
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
@@ -22,6 +29,21 @@ function MyApp({ Component, pageProps }) {
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
       </Head>
+
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${publicRuntimeConfig.GOOGLE_ANALYTICS_CODE}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${publicRuntimeConfig.GOOGLE_ANALYTICS_CODE}');
+        `}
+      </Script>
+
+      <NextNProgress height={4} color={publicRuntimeConfig.PROGRESSBAR_COLOR} />
       <Component {...pageProps} />
     </>
   );
